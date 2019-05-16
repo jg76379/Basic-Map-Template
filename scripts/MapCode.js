7db6460f261857a76e7ffd8ed279ceab2630b0da
James Gough <james.gough@sussexcountyde.gov>
2019-05-16 14:52:47 -0400

    Added legend and home widgets

--- scripts/MapCode.js
+++ scripts/MapCode.js
@@ -1,8 +1,13 @@
 require([
   "esri/Map",
   "esri/views/MapView",
+  "esri/widgets/Home",
+  "esri/widgets/Expand",
+  "esri/widgets/Legend",
   "dojo/domReady!"
-], function(Map, MapView) {
+], function(
+  Map, MapView, Home, Expand, Legend
+) {
 
   var map = new Map({
     basemap: "topo",
@@ -16,4 +21,18 @@ require([
     center: [-75.3, 38.7]
   });
 
+  var homeBtn = new Home({
+    view: view
+  });
+  view.ui.add(homeBtn, "top-left");
+
+  const legendExpand = new Expand({
+    view: view,
+    content: new Legend({
+      view: view,
+    }),
+    expandTooltip: "Expand Legend",
+    collapseTooltip: "Hide Legend"
+  });
+  view.ui.add(legendExpand, "top-left");
 });
